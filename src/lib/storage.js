2b1cf904554abb505aff9ca832846b5ec6a9798c
John Paul Ada <johnpaulada@gmail.com>
2019-08-10 16:03:30 +0800

    refactor: access token get to lib/ folder

---
+++ src/lib/storage.js
@@ -0,0 +1,9 @@
+// TODO: Implement
+export const isInStorage = key => async () => {
+  return false
+}
+
+// TODO: Implement
+export const getFromStorage = key => async () => {
+  return null
+}
--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,12 +1,25 @@
 import { call, put } from "redux-saga/effects"
+import { getFromStorage, isInStorage } from "../../lib/storage"
+
+const hasAccessTokenFromStorage = isInStorage("ACCESS_TOKEN")
+const getAccessTokenFromStorage = getFromStorage("ACCESS_TOKEN")
+
+const getAccessToken = async (username, password) => {
+  const inStorage = await hasAccessTokenFromStorage()
+  if (inStorage) {
+    const accessToken = await getAccessTokenFromStorage()
+    return accessToken
+  } else {
+    const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/auth`)
+    const { accessToken } = await response.json()
+
+    return accessToken
+  }
+}
 
 export function* login({ payload: { username, password } }) {
   try {
-    const response = yield call(
-      fetch,
-      `${process.env.REACT_APP_SERVER_URL}/auth`
-    )
-    const { accessToken } = yield call([response, "json"])
+    const accessToken = yield call(getAccessToken, username, password)
     yield put({ type: "LOGIN_SUCCESS", payload: { accessToken } })
   } catch (err) {
     console.log("== LOGIN_FAILED ==")
